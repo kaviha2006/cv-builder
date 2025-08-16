e5582787865de542e913d73a0f6d3177395dc076
Kaviha R. M <Kaviharavichandran2006@gmail.com>
2025-08-16 17:08:49 +0530

    Add vercel.json for proper routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-// App.js
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import TemplateSelection from "./pages/TemplateSelection";
 import CVForm from "./pages/CVForm";
@@ -8,7 +7,7 @@ import CVPreview from "./pages/CVPreview";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/cv-builder">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/template" element={<TemplateSelection />} />
